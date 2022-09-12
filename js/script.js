ef961ae113c1444e51647166a98260ef368b5176
Nali1919 <my@email.com>
2022-09-12 17:21:34 +0200

    Esercizio finale

--- js/script.js
+++ js/script.js
@@ -6,10 +6,11 @@ const app = new Vue(
         data: {
             text: 'Hello World',
             image: 'img/01.jpg',
-            image: 'img/02.jpg',
-            image: 'img/03.jpg',
-            image: 'img/04.jpg',
-            image: 'img/05.jpg'
+            image2: 'img/02.jpg',
+            image3: 'img/03.jpg',
+            image4: 'img/04.jpg',
+            image5: 'img/05.jpg',
+            classeH1: 'skin-h1'
 
         }
     }
