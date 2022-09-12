c0024ff5580ad1bd67a667a1fd8f3678c499cab4
Nali1919 <my@email.com>
2022-09-12 17:10:27 +0200

    Aggiungo base per immagini

--- js/script.js
+++ js/script.js
@@ -5,7 +5,12 @@ const app = new Vue(
         el: '#root',
         data: {
             text: 'Hello World',
-            image: 'img/01.jpg'
+            image: 'img/01.jpg',
+            image: 'img/02.jpg',
+            image: 'img/03.jpg',
+            image: 'img/04.jpg',
+            image: 'img/05.jpg'
+
         }
     }
 );
