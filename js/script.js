034dea3a11e8bb44b5e0a5ac8613933da991681c
Nali1919 <my@email.com>
2022-09-12 15:28:35 +0200

    Concludo esercizio

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+console.log('JS-OK')
+
 const app = new Vue(
-   {
-     el: '#root',
-     data: {
-        text : 'Ciao a tutti'
+    {
+        el: '#root',
+        data: {
+            text: 'Hello World',
+            image: 'img/01.jpg'
+        }
     }
-   }
 );
